118f78f925443a904cb2e274448814653f80b872
kayewong <kayewong@users.noreply.github.com>
2020-06-10 14:33:40 -0400

    Landing page first draft

---
+++ js/newcountdown.js
@@ -0,0 +1,27 @@
+<script>
+  function countdownTimer() {
+    const difference = +new Date("2020-07-31") - +new Date();
+    let remaining = "Time's up!";
+
+    if (difference > 0) {
+      const parts = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      };
+
+      remaining = Object.keys(parts)
+        .map(part => {
+          if (!parts[part]) return;
+          return `${parts[part]} ${part}`;
+        })
+        .join(" ");
+    }
+
+    document.getElementById("countdowntext").innerHTML = remaining;
+  }
+
+  countdownTimer();
+  setInterval(countdownTimer, 1000);
+</script>
